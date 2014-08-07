7d986800c9ba2e5668024e60a74be0f980865eb6
Yusuke KUOKA <yusuke.kuoka@crowdworks.co.jp>
2014-08-07 10:01:55 +0900

    fixes #11 The generator is not compatible with Rails 4: NoMethodError for `attr_accessible` on Rails 4 w/o protected_attributes gem

--- lib/feature/generators/install_generator.rb
+++ lib/feature/generators/install_generator.rb
@@ -12,12 +12,23 @@ module Feature
 
     def create_model_file
       template 'feature.rb', 'config/initializers/feature.rb'
-      template 'feature_toggle.rb', 'app/models/feature_toggle.rb'
+      template appropriate_feature_toggle_rb, 'app/models/feature_toggle.rb'
       migration_template 'create_feature_toggles.rb', 'db/migrate/create_feature_toggles.rb'
     end
 
     def self.next_migration_number(path)
       ActiveRecord::Generators::Base.next_migration_number(path)
+    end
+
+    private
+
+    def appropriate_feature_toggle_rb
+      # For Rails 4 + protected_attributes or Rails 3, we prefer the model with `attr_accessible` pre-populated
+      if ActiveRecord::Base.respond_to? :attr_accessible
+        'feature_toggle.rb'
+      else
+        'feature_toggle_without_attr_accessible.rb'
+      end
     end
   end
 end
