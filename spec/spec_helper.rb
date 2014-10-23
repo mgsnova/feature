b6b695dfa5feae325f0802141ab919a07afca568
mgsnova <github@mgsnova.de>
2014-10-23 16:54:03 +0200

    silence coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ require 'coveralls'
 require 'fakeredis/rspec'
 
 Coveralls.wear!
+Coveralls::Output.silent = true
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
