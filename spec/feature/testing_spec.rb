3d8ba07db7a05162ed305b44a58738078e2c9dba
Javid Jamae <javidjamae@gmail.com>
2015-01-12 15:21:49 -0800

    Fix bug where auto_refresh wipes out features when testing using run_with_deactivated and run_with_activated

--- spec/feature/testing_spec.rb
+++ spec/feature/testing_spec.rb
@@ -1,14 +1,7 @@
 require 'spec_helper'
 require 'feature/testing'
 
-describe 'Feature testing support' do
-  before(:all) do
-    repository = Feature::Repository::SimpleRepository.new
-    repository.add_active_feature(:active_feature)
-    repository.add_active_feature(:another_active_feature)
-    Feature.set_repository(repository)
-  end
-
+shared_examples "a testable repository" do
   before do
     expect(Feature.active?(:active_feature)).to be_truthy
     expect(Feature.active?(:another_active_feature)).to be_truthy
@@ -51,5 +44,29 @@ describe 'Feature testing support' do
         expect(Feature.active?(:another_active_feature)).to be_falsey
       end
     end
+  end
+end
+
+describe 'Feature testing support' do
+  context "without auto_refresh" do
+    before(:all) do
+      repository = Feature::Repository::SimpleRepository.new
+      repository.add_active_feature(:active_feature)
+      repository.add_active_feature(:another_active_feature)
+      Feature.set_repository(repository)
+    end
+
+    it_behaves_like "a testable repository"
+  end
+
+  context "with auto_refresh" do
+    before(:all) do
+      repository = Feature::Repository::SimpleRepository.new
+      repository.add_active_feature(:active_feature)
+      repository.add_active_feature(:another_active_feature)
+      Feature.set_repository(repository, true)
+    end
+
+    it_behaves_like "a testable repository"
   end
 end
