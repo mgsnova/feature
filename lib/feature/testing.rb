8d62ac88da845168fcb8b641a183ebe39cee475a
mgsnova <github@mgsnova.de>
2015-06-12 10:45:49 +0200

    minor refactoring

--- lib/feature/testing.rb
+++ lib/feature/testing.rb
@@ -13,16 +13,13 @@ module Feature
   #   Feature.run_with_activated(:feature, :another_feature) do
   #     # your test code here
   #   end
-  def self.run_with_activated(*features)
-    @active_features = [] if @active_features.nil?
-    old_features, old_auto_refresh, old_perform_initial_refresh =
-        @active_features.dup, @auto_refresh, @perform_initial_refresh
-    @active_features.concat(features).uniq!
-    @auto_refresh, @perform_initial_refresh = false, false
-    yield
-  ensure
-    @active_features, @auto_refresh, @perform_initial_refresh =
-        old_features, old_auto_refresh, old_perform_initial_refresh
+  def self.run_with_activated(*features, &blk)
+    with_stashed_config do
+      @active_features.concat(features).uniq!
+      @auto_refresh = false
+      @perform_initial_refresh = false
+      blk.call
+    end
   end
 
   # Execute the code block with the given features deactive
@@ -31,15 +28,26 @@ module Feature
   #   Feature.run_with_deactivated(:feature, :another_feature) do
   #     # your test code here
   #   end
-  def self.run_with_deactivated(*features)
+  def self.run_with_deactivated(*features, &blk)
+    with_stashed_config do
+      @active_features -= features
+      @auto_refresh = false
+      @perform_initial_refresh = false
+      blk.call
+    end
+  end
+
+  # Execute the given code block and store + restore the feature
+  # configuration before/after the execution
+  def self.with_stashed_config
     @active_features = [] if @active_features.nil?
-    old_features, old_auto_refresh, old_perform_initial_refresh =
-        @active_features.dup, @auto_refresh, @perform_initial_refresh
-    @active_features -= features
-    @auto_refresh, @perform_initial_refresh = false, false
+    old_features = @active_features.dup
+    old_auto_refresh = @auto_refresh
+    old_perform_initial_refresh = @perform_initial_refresh
     yield
   ensure
-    @active_features, @auto_refresh, @perform_initial_refresh =
-        old_features, old_auto_refresh, old_perform_initial_refresh
+    @active_features = old_features
+    @auto_refresh = old_auto_refresh
+    @perform_initial_refresh = old_perform_initial_refresh
   end
 end
