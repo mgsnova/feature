e5a56bf41c921f12525dc9dd50702a2bdcb91f5d
Will Bryant <william@tout.com>
2016-04-30 18:10:10 -0700

    allow for the setting of a custom redis client

--- lib/feature/repository/redis_repository.rb
+++ lib/feature/repository/redis_repository.rb
@@ -10,12 +10,15 @@ module Feature
     # the Redis hash that will store all of your feature toggles.
     #
     class RedisRepository
+      attr_writer :redis
+      
       # Constructor
       #
       # @param redis_key the key of the redis hash where all the toggles will be stored
       #
-      def initialize(redis_key)
+      def initialize(redis_key, client=nil)
         @redis_key = redis_key
+        @redis = client unless client.nil?
       end
 
       # Returns list of active features
@@ -23,7 +26,7 @@ module Feature
       # @return [Array<Symbol>] list of active features
       #
       def active_features
-        Redis.current.hgetall(@redis_key).select { |_k, v| v.to_s == 'true' }.map { |k, _v| k.to_sym }
+        redis.hgetall(@redis_key).select { |_k, v| v.to_s == 'true' }.map { |k, _v| k.to_sym }
       end
 
       # Add an active feature to repository
@@ -33,7 +36,7 @@ module Feature
       def add_active_feature(feature)
         check_feature_is_not_symbol(feature)
         check_feature_already_in_list(feature)
-        Redis.current.hset(@redis_key, feature, true)
+        redis.hset(@redis_key, feature, true)
       end
 
       # Checks that given feature is a symbol, raises exception otherwise
@@ -51,9 +54,18 @@ module Feature
       # @param [Symbol] feature the feature to be checked
       #
       def check_feature_already_in_list(feature)
-        raise ArgumentError, "feature :#{feature} already added" if Redis.current.hexists(@redis_key, feature)
+        raise ArgumentError, "feature :#{feature} already added" if redis.hexists(@redis_key, feature)
       end
       private :check_feature_already_in_list
+
+      # Returns the currently specified redis client
+      #
+      # @return [Redis] Currently set redis client
+      #
+      def redis
+        @redis ||= Redis.current
+      end
+      private :redis
     end
   end
 end
